967b7cac632acf3d173b291644668f9b74018f2c
Alex Morling <atmorling@gmail.com>
2025-08-10 22:16:36 +0200

    first cut port widgets

---
+++ src/legend.tsx
@@ -0,0 +1,85 @@
+import {
+  Deck,
+  Widget,
+  WidgetPlacement,
+} from "@deck.gl/core";
+
+export type LegendWidgetProps = {
+  id: string;
+  title: string;
+  legend: Map<string, string>;
+  placement?: WidgetPlacement;
+  style?: Partial<CSSStyleDeclaration>;
+  className?: string;
+}
+
+export default class LegendWidget implements Widget<LegendWidgetProps> {
+  id = "legend";
+  props: LegendWidgetProps;
+  placement: WidgetPlacement = "bottom-right";
+
+  deck?: Deck;
+  element?: HTMLDivElement;
+  className: string = "deck-widget-legend";
+
+  constructor(props: LegendWidgetProps) {
+    this.id = props.id || "legend";
+    this.placement = props.placement || "bottom-right";
+    this.className = props.className || "deck-widget-legend";
+    this.props = props;
+    this.props.style = this.props.style || {};
+    this.props.title = this.props.title || "Legend";
+  }
+
+  setProps(props: Partial<LegendWidgetProps>) {
+    Object.assign(this.props, props);
+  }
+
+  onAdd({ deck }: { deck: Deck }): HTMLDivElement {
+    const element = document.createElement("div");
+
+    element.classList.add("deck-widget");
+    if (this.className) element.classList.add(this.className);
+
+    const titleElement = document.createElement("div");
+    titleElement.innerText = this.props.title;
+    titleElement.classList.add("legend-title");
+
+    const legendElement = document.createElement("div");
+    legendElement.classList.add("legend-scale");
+
+    const ul = document.createElement("ul");
+    ul.classList.add("legend-labels");
+
+    this.props.legend.forEach((color, label) => {
+      const li = document.createElement("li");
+      const span = document.createElement("span");
+
+      span.style.setProperty("background", color);
+      li.innerText = label;
+
+      li.appendChild(span);
+      ul.appendChild(li);
+    });
+
+    legendElement.appendChild(ul);
+
+    const { style } = this.props;
+    if (style) {
+      Object.entries(style).map(([key, value]) => {
+        element.style.setProperty(key, value as string);
+      });
+    }
+    element.appendChild(titleElement);
+    element.appendChild(legendElement);
+
+    this.deck = deck;
+    this.element = element;
+    return element;
+  }
+
+  onRemove() {
+    this.deck = undefined;
+    this.element = undefined;
+  }
+}
---
+++ src/save-image.tsx
@@ -0,0 +1,115 @@
+import {
+  Deck,
+  Widget,
+  WidgetPlacement,
+} from "@deck.gl/core";
+import { toPng } from "html-to-image";
+import {render} from 'preact';
+
+interface SaveImageWidgetProps {
+  id: string;
+  label?: string;
+
+  placement?: WidgetPlacement;
+  style?: Partial<CSSStyleDeclaration>;
+  className?: string;
+}
+
+export default class SaveImageWidget implements Widget<SaveImageWidgetProps> {
+  id = "save-image";
+  props: SaveImageWidgetProps;
+  placement: WidgetPlacement = "top-right";
+  viewId?: string | null = null;
+  deck?: Deck<any>;
+  element?: HTMLDivElement;
+
+  constructor(props: SaveImageWidgetProps) {
+    this.id = props.id || "save-image";
+    this.placement = props.placement || "top-right";
+    props.label = props.label || "Save as Image";
+    props.style = props.style || {};
+    this.props = props;
+  }
+
+  onAdd({ deck }: { deck: Deck<any> }): HTMLDivElement {
+    const { style, className } = this.props;
+    const element = document.createElement("div");
+    element.classList.add("deck-widget", "deck-widget-save-image");
+    if (className) element.classList.add(className);
+    if (style) {
+      Object.entries(style).map(([key, value]) =>
+        element.style.setProperty(key, value as string),
+      );
+    }
+
+    const ui = (
+      <div className="deck-widget-button">
+        <button
+          className={`deck-widget-icon-button ${className}`}
+          type="button"
+          onClick={this.handleClick.bind(this)}
+          title={this.props.label}
+        >
+          <svg
+            fill="#000000"
+            version="1.1"
+            width="85%"
+            height="85%"
+            viewBox="0 0 492.676 492.676"
+          >
+            <g>
+              <g>
+                <path
+                  d="M492.676,121.6H346.715l-23.494-74.789h-40.795H210.25h-40.794L145.961,121.6H0v324.266h492.676V121.6L492.676,121.6z
+                M246.338,415.533c-72.791,0-131.799-59.009-131.799-131.799c0-72.792,59.008-131.8,131.799-131.8s131.799,59.008,131.799,131.8
+                C378.137,356.525,319.129,415.533,246.338,415.533z"
+                />
+                <path
+                  d="M246.338,199.006c-46.72,0-84.728,38.008-84.728,84.729c0,46.72,38.008,84.728,84.728,84.728
+                c46.721,0,84.728-38.008,84.728-84.728C331.065,237.014,293.059,199.006,246.338,199.006z"
+                />
+              </g>
+            </g>
+          </svg>
+        </button>
+      </div>
+    );
+
+    this.deck = deck;
+    this.element = element;
+    render(ui, element);
+    return element;
+  }
+
+  async handleClick() {
+    if (this.deck) {
+      this.deck.redraw("true");
+      const deck_wrapper = this.deck?.getCanvas()?.parentElement;
+
+      if (deck_wrapper) {
+        toPng(deck_wrapper)
+          .then(function (dataUrl) {
+            const img = new Image();
+            img.src = dataUrl;
+
+            const a = document.createElement("a");
+            a.href = dataUrl;
+            a.download = "map.png";
+            a.click();
+          })
+          .catch(function (error) {
+            console.error("Failed to export PNG", error);
+          });
+      }
+    }
+  }
+
+  onRemove() {
+    this.deck = undefined;
+    this.element = undefined;
+  }
+
+  setProps(props: Partial<SaveImageWidgetProps>) {
+    Object.assign(this.props, props);
+  }
+}
