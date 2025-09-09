5a5fdeaba90f759d0ab51eee34ded696838e09e8
Alex Morling <atmorling@gmail.com>
2025-09-10 09:08:59 +1000

    add tiled bitmap layer

--- src/index.ts
+++ src/index.ts
@@ -3,16 +3,19 @@ import TitleWidget from './title';
 import ScaleWidget from './scale';
 import LegendWidget from './legend';
 import SaveImageWidget from './save-image';
+import TiledBitmapLayer from './tiled-bitmap-layer';
 
 export {default as NorthArrowWidget} from './na-widget'
 export {default as TitleWidget} from './title'
 export {default as ScaleWidget} from './scale'
 export {default as LegendWidget} from './legend'
 export {default as SaveImageWidget} from './save-image';
+export {default as TiledBitmapLayer} from './tiled-bitmap-layer';
 
 const _global = typeof window === 'undefined' ? global : window;
 _global.NorthArrowWidget = {NorthArrowWidget};
-_global.NorthArrowWidget = {TitleWidget};
-_global.NorthArrowWidget = {ScaleWidget};
-_global.NorthArrowWidget = {LegendWidget};
-_global.NorthArrowWidget = {SaveImageWidget};
+_global.TitleWidget = {TitleWidget};
+_global.ScaleWidget = {ScaleWidget};
+_global.LegendWidget = {LegendWidget};
+_global.SaveImageWidget = {SaveImageWidget};
+_global.TiledBitmapLayer = {TiledBitmapLayer};
